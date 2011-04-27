d099908cbdf8f998c01f57c1519d32d719682774
OrigamiTech <origamitech@gmail.com>
2011-04-27 23:01:24 +0100

    split IRCClient across several files for ease of reading

---
+++ IRCClient_EventHandlers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Absinthe
+{
+    public partial class IRCClient
+    {
+        public event EventHandler_GET_PRIVMSG GET_PRIVMSG;
+        public event EventHandler_RPL_ENDOFMOTD RPL_MOTDSTART;
+        public event EventHandler_RPL_ENDOFMOTD RPL_MOTD;
+        public event EventHandler_RPL_ENDOFMOTD RPL_ENDOFMOTD;
+        public delegate void EventHandler_GET_PRIVMSG(object sender, EventArgs e);
+        public delegate void EventHandler_RPL_MOTDSTART(object sender, EventArgs e);
+        public delegate void EventHandler_RPL_MOTD(object sender, EventArgs e);
+        public delegate void EventHandler_RPL_ENDOFMOTD(object sender, EventArgs e);
+        protected virtual void OnGET_PRIVMSG(EventArgs e) { if(GET_PRIVMSG != null)GET_PRIVMSG(this, e); }
+        protected virtual void OnRPL_MOTDSTART(EventArgs e) { if(RPL_MOTDSTART != null) RPL_MOTDSTART(this, e); }
+        protected virtual void OnRPL_MOTD(EventArgs e) { if(RPL_MOTD != null) RPL_MOTD(this, e); }
+        protected virtual void OnRPL_ENDOFMOTD(EventArgs e) { if(RPL_ENDOFMOTD != null) RPL_ENDOFMOTD(this, e); }
+    }
+}
---
+++ IRCClient_IRCCommands.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Absinthe
+{
+    public partial class IRCClient
+    {
+        public void JOIN(string channel)
+        {
+            JOIN(new Dictionary<string, string>() { { channel, "" } }, false);
+        }
+        public void JOIN(string channel, string key)
+        {
+            JOIN(new Dictionary<string, string>() { { channel, key } }, false);
+        }
+        public void JOIN(Dictionary<string, string> channels, bool zero)
+        {
+            if(zero)
+            {
+                writer.WriteLine(CommandCode.JOIN + " 0");
+                return;
+            }
+            string channel = "";
+            string key = "";
+            bool first = true;
+            foreach(KeyValuePair<string, string> pair in channels)
+            {
+                channel += (first ? "" : ",") + pair.Key;
+                key += (first ? "" : ",") + pair.Value;
+                first = false;
+            }
+            writer.WriteLine(CommandCode.JOIN + " " + channel + " " + key);
+        }
+        public void NICK(string nickname)
+        {
+            writer.WriteLine(CommandCode.NICK + " " + nickname);
+        }
+        public void PING(string param)
+        {
+            writer.WriteLine(CommandCode.PING + (param != "" ? " :" + param : ""));
+        }
+        public void PONG(string param)
+        {
+            writer.WriteLine(CommandCode.PONG + (param != "" ? " :" + param : ""));
+        }
+        public void PRIVMSG(string[] receivers, string message)
+        {
+            if(receivers.Length == 0)
+                return;
+            string receiver = receivers[0];
+            for(int i = 1; i < receivers.Length; i++)
+                receiver += "," + receivers[i];
+            writer.WriteLine(CommandCode.PRIVMSG + " " + receiver + " :" + message);
+        }
+        public void USER(string username, bool invisible, string realname)
+        {
+            writer.WriteLine(CommandCode.USER + " " + username + " " + (invisible ? "8" : "0") + " * :" + realname);
+        }
+    }
+}
